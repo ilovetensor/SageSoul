d3f01b182a02b709ab3913436932e901ad175164
Anjali <holla2467@gmail.com>
2023-12-12 11:29:10 +0530

    added database model and 5 entries

--- SageSoulProject/app/models.py
+++ SageSoulProject/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class DataModel(models.Model):
+    herb = models.CharField(max_length=100)
+    name = models.TextField(blank=True)
+    hindi_name = models.TextField(blank=True)
+    english_name = models.TextField(blank=True)
+    properties_rasa = models.TextField()
+    properties_guna = models.TextField()
+    properties_virya = models.TextField()
+    properties_vipaka = models.TextField()
+    effects_on_dosa = models.TextField()
+    caution = models.TextField(blank=True)
+
